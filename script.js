96833d1fb9425a79f5cdc78476f80c9b16a9b39b
praseesh <praseeshprasee@gmail.com>
2024-02-06 10:16:36 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function buttonClick(val){
+    document.getElementById("screen").value+=val
+}
+function clearDisplay(){
+    document.getElementById("screen").value=""
+}
+function equalClick(){
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById('screen').value=result
+}
